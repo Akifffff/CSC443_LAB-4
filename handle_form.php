d4f894644a3ff934e9e3ea8b5a9229c4d29cb0c0
Akifffff <abdakif12@gmail.com>
2022-05-11 23:42:24 +0800

    commit message

--- handle_form.php
+++ handle_form.php
@@ -6,18 +6,11 @@
 <body>
 <?php #Script handle form
 
-if (isset ($_POST["gender"])) {
-    if($_POST["gender"] == 'M') {
-
-        $message = '<b><p>Good Day, Sir </b></p>' ;
-    }
-    if($_POST["gender"] == 'F' ) {
-        $message = '<b><p>Good Day, Madam </b></p>' ;
-    }
-}else{
-    $_POST["gender"] = null;
-    
-    echo '<p><b> You Forgot to Choose Your Gender! </b></p>' ;
+if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) {
+    echo "Thank you," $_POST["name"]. " for the following comments: <br /> <tt>". 
+    $_POST["comments"]. " </tt> <p> We will reply you at <i>" . $_POST["email"].
+    "</i></p>";
+    echo $message;
 }
 
 
