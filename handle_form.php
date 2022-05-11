95d070c783261da0bedb727401a643488f6d5f08
Akifffff <abdakif12@gmail.com>
2022-05-11 23:44:14 +0800

    commit message

--- handle_form.php
+++ handle_form.php
@@ -7,7 +7,7 @@
 <?php #Script handle form
 
 if ($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) {
-    echo "Thank you," $_POST["name"]. " for the following comments: <br /> <tt>". 
+    echo "Thank you," .$_POST["name"]. " for the following comments: <br /> <tt>". 
     $_POST["comments"]. " </tt> <p> We will reply you at <i>" . $_POST["email"].
     "</i></p>";
     echo $message;
